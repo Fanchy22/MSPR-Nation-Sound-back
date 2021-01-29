302dde161a676cd9384b2daf944070d9ba27c3b4
Fanchy22 <73825990+Fanchy22@users.noreply.github.com>
2021-01-29 15:35:28 +0100

    artiste réalisé

--- src/Controller/ArtisteController.php
+++ src/Controller/ArtisteController.php
@@ -4,19 +4,48 @@ namespace App\Controller;
 
 use App\Entity\Artiste;
 use App\Form\ArtisteType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ArtisteController extends AbstractController
 {
     /**
      * @Route("/artiste", name="artiste")
      */
-    public function index(): Response
+    public function index(Request $request,SluggerInterface $slugger,EntityManagerInterface $entityManager): Response
     {
         $artiste = new Artiste();
         $artisteForm = $this->createForm(ArtisteType::class, $artiste);
+        $artisteForm->handleRequest($request);
+
+        if ($artisteForm->isSubmitted() && $artisteForm->isValid()) {
+            /** @var UploadedFile $profileFile */
+            $profileFile = $artisteForm->get('profile')->getData();
+            if ($profileFile) {
+                $originalFilename = pathinfo($profileFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$profileFile->guessExtension();
+                // Move the file to the directory where profileFile are stored
+                try {
+                    $profileFile->move(
+                        $this->getParameter('profiles_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $artiste->setThumbnail($newFilename);
+            }
+            //insérer en base de données $artiste
+            $entityManager->persist($artiste);
+            $entityManager->flush();
+        }
 
         return $this->render('artiste/index.html.twig',
            [
--- src/Form/ArtisteType.php
+++ src/Form/ArtisteType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Artiste;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,16 @@ class ArtisteType extends AbstractType
         $builder
             ->add('name')
             ->add('genre')
-            ->add('thumbnail')
+            ->add('profile', FileType::class, [
+                'label' => 'Image de profil (ou de face)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+                ])
         ;
     }
 
